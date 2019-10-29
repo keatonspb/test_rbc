104c34d26f6ab45709c1b56f52b5cdad40cff4b2
Konstantin Bychkovskiy <k.bychkovskiy@kingfin.com>
2019-10-29 16:51:44 +0300

    Grabber fix

--- commands/GrabberController.php
+++ commands/GrabberController.php
@@ -10,51 +10,59 @@ use Symfony\Component\DomCrawler\Crawler;
 use yii\console\Controller;
 use yii\console\ExitCode;
 
-
+/**
+ * Контроллер парсинга
+ * Class GrabberController
+ * @package app\commands
+ */
 class GrabberController extends Controller
 {
     const BASE_URL = "https://www.rbc.ru/";
-    private static $base_headers = [
+    const BASE_HEADERS = [ //Подменем заголовки
+        'Origin' => "https://www.rbc.ru/",
         'User-Agent' => "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
         "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     ];
     private $Client;
 
+    /**
+     * Парсит новости c главной
+     * @return int
+     */
     public function actionIndex()
     {
         $client = $this->getClient();
-        $response = $client->get("/");
+        $response = $client->get("/"); //Получаем исходный код главной страницы
         $crawler = new Crawler($response->getBody()->getContents());
-        $crawler->filterXPath("//div[contains(@class, 'js-news-feed-list')]/a[contains(@class, 'news-feed__item')]")
-            ->each(function (Crawler $node, $i) {
+        $crawler->filterXPath("//div[contains(@class, 'js-news-feed-list')]/a[contains(@class, 'news-feed__item')]") //Ищем новости через xpath
+            ->each(function (Crawler $node) {
                 $link = $node->attr("href");
-
-                if (!$link) {
+                if (!$link) { //Нам не нада новость со ссылкой
                     return;
                 }
                 $titleNode = $node->filterXPath("//span[contains(@class, 'news-feed__item__title')]")->first();
-                if (!$titleNode->count()) {
+                if (!$titleNode->count()) { //Если нод нет
                     return;
                 }
                 $title = trim($titleNode->text());
 
-                $id = str_replace("id_newsfeed_", "", $node->attr("id"));
+                $id = str_replace("id_newsfeed_", "", $node->attr("id")); //Получаем внешний id
                 if ($id) {
-                    if (!($Article = Article::find()->where(['rbc_hash' => $id])->one())) {
+                    if (!($Article = Article::find()->where(['rbc_hash' => $id])->one())) { // Проверяем, есть ли такая статья
                         $Article = new Article();
                         $Article->rbc_hash = $id;
                     }
-                } else {
-                    $Article = new Article();
+                } else { //Статья без id нам тоже не нужна
+                    return;
                 }
 
-                $Article->modified_at = date("Y-m-d H:i:s", $node->attr("data-modif") ?? time());
-                echo $title . " " . $link . " ". $Article->modified_at . PHP_EOL;
+                $Article->modified_at = date("Y-m-d H:i:s", $node->attr("data-modif") ?? time()); //Получаем дату статьи
+                $this->stdout($title . " " . $link . " ". $Article->modified_at . PHP_EOL);
                 $Article->link = $link;
                 $Article->title = $title;
 
                 $this->getArticleContent($link, $Article);
-                if($Article->content) {
+                if($Article->content) { //Нам нужна статья с контентом
                     $Article->save();
                 }
 
@@ -63,12 +71,16 @@ class GrabberController extends Controller
 
     }
 
+    /**
+     * HTTP клиент
+     * @return Client
+     */
     private function getClient(): Client
     {
         if (!$this->Client) {
             $this->Client = new Client([
                 'base_uri' => self::BASE_URL,
-                'headers' => self::$base_headers,
+                'headers' => self::BASE_HEADERS,
             ]);
         }
         return $this->Client;
@@ -78,20 +90,18 @@ class GrabberController extends Controller
     {
         $this->stdout("Gettiing ".$url.PHP_EOL);
         $client = $this->getClient();
-        $response = $client->get($url);
+        $response = $client->get($url); //Получаем исходный код статьи
         $crawler = new Crawler($response->getBody()->getContents());
-        $articleBlock = $crawler->filterXPath("//div[@itemprop='articleBody']")->first();
+        $articleBlock = $crawler->filterXPath("//div[@itemprop='articleBody']")->first(); //Ищем блок с контентом. Материал без метаданных является партнерским
         if ($articleBlock->count()) {
             $article->content = trim($articleBlock->html());
         }
 
-        $imageBlock = $crawler->filterXPath("//mata[@name='twitter:image']")->first();
+        $imageBlock = $crawler->filterXPath("//meta[@property='og:image']")->first(); //Берем картинку из метатэга opengraph
         if ($imageBlock->count()) {
-            $article->image_link = trim($imageBlock->html());
+            $this->stdout("Image ".trim($imageBlock->attr("content")).PHP_EOL);
+            $article->image_link = trim($imageBlock->attr("content"));
         }
-
-
     }
-
 
 }
